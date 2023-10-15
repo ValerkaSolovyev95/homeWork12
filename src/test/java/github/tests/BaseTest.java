b0f83fdb9cfd4bb34412b89a6c206ca2604ea4e0
VA.Solovev <va.solovev@tinkoff.ru>
2023-10-15 17:17:06 +0300

    fix add capabilities, move SelenideLogger.addListener in BeforeEach

--- src/test/java/github/tests/BaseTest.java
+++ src/test/java/github/tests/BaseTest.java
@@ -7,10 +7,14 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.EroshenkoRepositoryPage;
 import pages.IssuesPage;
 import pages.MainPage;
 import pages.RepositoryPage;
+
+import java.util.Map;
 
 public class BaseTest {
 
@@ -24,6 +28,16 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://github.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addLogger() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
